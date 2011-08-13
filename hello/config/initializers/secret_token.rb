c0092355046fc9eff9d1729ffb1657f2efabe5f8
Gordon McLachlan <gordon@primate.co.uk>
2011-08-13 15:48:25 +0100

    hello world example

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '7c2d99d763476dff60b808ac611d18685c112f40252d00d0daf284acee68455fcd116d697654ab753c34c8581a16b6662fd04a86aa2d26a218fef6e7eada1f53'
