e5f8f9b0d80aec87273128eef3281dd71c19b01a
Gordon McLachlan <gordon@primate.co.uk>
2011-08-20 21:36:12 +0100

    More blog work from book

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+user = User.create :email => 'gordon@primate.co.uk', :password => 'primate'
+Category.create [
+  {:name => 'Programming'}, 
+  {:name => 'Event'}, 
+  {:name => 'Travel'}, 
+  {:name => 'Music'}, 
+  {:name => 'TV'}
+  ]
