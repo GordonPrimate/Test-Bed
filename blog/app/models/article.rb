febbe41124480a5bfc4e9a4c5f8c19f238066c82
Gordon McLachlan <gordon@primate.co.uk>
2011-08-22 17:14:19 +0100

    Continuing with book examples

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -6,6 +6,9 @@ class Article < ActiveRecord::Base
    has_and_belongs_to_many :categories
    has_many :comments
    
+   scope :published, where("articles.published.at IS NOT NULL")
+   scope :draft, where("articles.published.at IS NULL")
+   
    def long_title
      "#{title} - #{published_at}"
    end
