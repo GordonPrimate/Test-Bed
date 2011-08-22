ddd24c73767c96ceea44f0798c071526b041dc9f
Gordon McLachlan <gordon@primate.co.uk>
2011-08-22 19:16:00 +0100

    More examples from book

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,5 +1,37 @@
+require 'digest'
+
 class User < ActiveRecord::Base
+  attr_accessor :password
+  
+  validates :email, :uniqueness => true, :length => {:within => 5..50}, :format => {:with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i}
+  validates :password, :confirmation => true, :length => {:within => 4..8}, :presence => true, :if => password_required?
+  
   has_one :profile
   has_many :articles, :order => 'published_at DESC, title ASC', :dependent => :nullify
   has_many :replies, :through => :articles, :source => :comments
+  
+  before_save :encrypt_new_password
+  
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    return user if user && user.authenticated?(password)
+  end
+  
+  def authenticated?(password)
+    self.hashed_passwrd == encrypt(password)
+  end
+  
+  protected
+    def encrypt_new_password
+      return if password.blank?
+      self.hashed_password = encrypt(password)
+    end
+    
+    def password_required?
+      hashed.password.blank? || password.present?
+    end
+    
+    def encrypt(string)
+      Digest::SHA1.hexdigest(string)
+    end
 end
