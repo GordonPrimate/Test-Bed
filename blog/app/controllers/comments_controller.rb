440cf952b4171bddde9d4b60b4e34debf45a3521
Gordon McLachlan <gordon@primate.co.uk>
2011-08-25 14:13:59 +0100

    More work

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,23 +1,24 @@
 class CommentsController < ApplicationController
-  before_filter :load_article
+  #before_filter :load_article
   
   def create
-    @comment = @articles.comment.new(params[:comment])
-    if @comment.save
-      redirect_to @article, :notice => 'Thanks for your comment'
+    comment = Comment.new(params[:comment])
+    comment.article = Article.find(params[:article_id])
+    if comment.save
+      redirect_to comment.article, :notice => 'Thanks for your comment'
     else
-      redirect_to @article, :alert => 'Unable to add comment'
+      redirect_to comment.article, :alert => 'Unable to add comment'
     end
   end
   
   def destroy
-    @comment = @articles.comment.find(params[:id])
+    @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to @article, :notice => 'Comment deleted'
   end
   
-  private
-    def load_articles
-      @article = Articles.find(params[:article_id])
-    end
+  #private
+  #  def load_article
+  #    @article = Article.find(params[:article_id])
+  #  end
 end
